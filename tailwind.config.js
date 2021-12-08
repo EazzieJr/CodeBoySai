8395df223263487dc796c26aa3bac8f881e13ce1
Ganiyu Idris <golamide27@gmail.com>
2021-12-08 22:50:02 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+        cormorant: ['Cormorant Garamond'],
+      },
+      fontSize: {
+        'tiny': '.6rem',
+        'lg3xl': '2rem',
+        '4xl': '2.25rem',
+        'base4xl': '2.4rem',
+        'lg4xl': '2.5rem',
+        '5xl': '3rem',
+        'lg5xl': '3.5rem',
+        '6xl': '4rem',
+        'lg6xl': '4.5rem',
+        '7xl': '5rem',
+        'lg7xl': '5.5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+        '10xl': '8rem',
+        '11xl': '10rem',
+        '12xl': '12rem',
+        '13xl': '13rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
